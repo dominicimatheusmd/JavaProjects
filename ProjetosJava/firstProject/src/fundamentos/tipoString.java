e5765601aabec574218e61aeaec19ccab9cfa228
dominici10 <matheus.e@aluno.faculdadeimpacta.com.br>
2023-12-27 21:16:19 -0300

    "Alteração pra ver se sobe"

--- ProjetosJava/firstProject/src/fundamentos/tipoString.java
+++ ProjetosJava/firstProject/src/fundamentos/tipoString.java
@@ -7,18 +7,18 @@ public class tipoString {
         System.out.println(s.concat("!!!")); //Concatenar com função
         System.out.println(s + "!!!"); //Concatenar com +
 
-        System.out.println(s.startsWith("Boa")); //função startsWith vai ferificar se a String s contém a condição informada
-        System.out.println(s.toLowerCase().startsWith("boa")); // toLowerCase() irá deixar a String de s minuscula
+        //System.out.println(s.startsWith("Boa")); //função startsWith vai ferificar se a String s contém a condição informada
+        //System.out.println(s.toLowerCase().startsWith("boa")); // toLowerCase() irá deixar a String de s minuscula
         System.out.println(s.toUpperCase().endsWith("TARDE")); // toUpperCase() irá deixar a String s em maiuscula e a endsWith verifica se o texto termina com a condição informada
 
         System.out.println(s.length()); //verifica o tamanho da String informada
-        System.out.println(s.toLowerCase().equals("boa tarde")); //equals retorna True ou False se o texto anterior for igual a condição informada
-        System.out.println(s.equalsIgnoreCase("boa tarde")); //Não considera letras maiusculas ou minusculas
+        //System.out.println(s.toLowerCase().equals("boa tarde")); //equals retorna True ou False se o texto anterior for igual a condição informada
+        //System.out.println(s.equalsIgnoreCase("boa tarde")); //Não considera letras maiusculas ou minusculas
 
         var nome = "Matheus";
         var sobrenome = "Dominici";
         var idade = 21;
-        var salario = 20000.6452;
+        var salario = 20000.6454;
 
         String maisUmaFrase = "Nome: " + nome + "\nSobrenome: " + sobrenome + "\nIdade: " + idade + "\nSalario: " + salario;
 
@@ -29,7 +29,7 @@ public class tipoString {
         String frase = String.format("O senhor %s %s tem %d anos e recebe R$ %.2f de salário", nome, sobrenome, idade, salario);
         System.out.println(frase);
 
-        System.out.println("Frase qualquer".contains("qual")); // verifica se o texto contem a condição informada
+        //System.out.println("Frase qualquer".contains("qual")); // verifica se o texto contem a condição informada
         System.out.println("Frase qualquer".indexOf("qual"));// retorna em qual index a condição informada está
         System.out.println("Frase qualquer".substring(6, 8));// retorna o texto que estiver no index da condição passada
 
