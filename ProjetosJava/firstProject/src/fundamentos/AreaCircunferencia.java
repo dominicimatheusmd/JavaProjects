7abf1512c074998a5d7f4a29ff907aac46c7a8d7
dominicimatheusmd <dominicimatheus0@gmail.com>
2024-01-02 09:29:39 -0300

    Alteracao para teste

--- ProjetosJava/firstProject/src/fundamentos/AreaCircunferencia.java
+++ ProjetosJava/firstProject/src/fundamentos/AreaCircunferencia.java
@@ -2,7 +2,7 @@ package fundamentos;
 
 public class AreaCircunferencia {
     public static void main(String[] args) {
-        int raio = 3;
+        int raio = 9;
         final double pi = 3.14;
 
         raio = 9;
