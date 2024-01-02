21d0bf9dc33da8df7142b4f246deb152d07bdf19
dominicimatheusmd <dominicimatheus0@gmail.com>
2024-01-02 09:25:24 -0300

    Alteração na area para 3

--- ProjetosJava/firstProject/src/fundamentos/AreaCircunferencia.java
+++ ProjetosJava/firstProject/src/fundamentos/AreaCircunferencia.java
@@ -2,7 +2,7 @@ package fundamentos;
 
 public class AreaCircunferencia {
     public static void main(String[] args) {
-        int raio = 9;
+        int raio = 3;
         final double pi = 3.14;
 
         raio = 9;
