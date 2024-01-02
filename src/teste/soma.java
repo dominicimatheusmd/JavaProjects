1cb98926de806714fa3068caf5508d198d638e02
dominicimatheusmd <dominicimatheus0@gmail.com>
2024-01-02 20:58:17 -0300

    Teste projeto novo

---
+++ src/teste/soma.java
@@ -0,0 +1,15 @@
+package teste;
+
+public class soma {
+    public static void main(String[] args) {
+        int january = 15;
+        int february = 23;
+        int march = 17;
+
+        int soma = january + february + march;
+        double divion = soma / 3;
+        System.out.printf("O valor do trimestre foi de R$" + soma + "mil reais. E a média mensal foi de R$" + divion + "mil reais" );
+
+
+    }
+}
---
+++ src/teste/switchTeste.java
@@ -0,0 +1,23 @@
+package teste;
+
+import java.util.*;
+public class switchTeste {
+    public static void main(String[] args) {
+        System.out.println("Insira um numero de 1 a 10");
+        Scanner scan = new Scanner (System.in);
+        int numero = scan.nextInt();
+        switch (numero){
+            case 1:
+                System.out.printf("Numero inserido foi 1!!");
+                break;
+            case 10:
+                System.out.printf("Numero inserido foi 10!!");
+                break;
+            case 100:
+                System.out.printf("Numero inserido foi 100!!");
+                break;
+            default:
+                System.out.printf("numero fora do range, vsf tmj!!");
+        }
+    }
+}
